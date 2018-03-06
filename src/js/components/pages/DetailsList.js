49a0e88cad8effc005533f8e6d566a466ec0e8d0
Nedjim <nedjim.danimon@gmail.com>
2018-03-06 15:25:42 +0100

    update style

--- src/js/components/pages/DetailsList.js
+++ src/js/components/pages/DetailsList.js
@@ -3,18 +3,22 @@ import React from 'react'
 const DetailsList = (data) => {
   return (
     <div>
-      <h3>Comics</h3>
-      <ul>
-        { data.comics.map((comic) => {
-          return (<li>{comic.title}</li>)
-        })}
-      </ul>
-      <h3>Series</h3>
-      <ul>
-        { data.series.map((serie) => {
-          return (<li>{serie.title}</li>)
-        })}
-      </ul>
+      <div id="comics" className="details-list">
+        <h3>Comics</h3>
+        <ul>
+          { data.comics.map((comic) => {
+            return (<li>{comic.title}</li>)
+          })}
+        </ul>
+      </div>
+      <div id="series" className="details-list">
+        <h3>Series</h3>
+        <ul>
+          { data.series.map((serie) => {
+            return (<li>{serie.title}</li>)
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
